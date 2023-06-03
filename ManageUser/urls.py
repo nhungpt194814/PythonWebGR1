92e0912dfe176faec5b29af736fb843097cf74c0
nhungpt194814 <nhung.pt194814@gmail.com>
2023-06-03 22:56:10 +0700

    add user_list in ManageUser

--- ManageUser/urls.py
+++ ManageUser/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login')
+    path('login/', views.user_login, name='login'),
+    path('user_list/', views.user_login, name='login')
+
 ]
